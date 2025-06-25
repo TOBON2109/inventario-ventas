6923b4ed3c7c67f89e97ac37f7e390a5009afde9
TOBON2109 <juanestobondiaz@gmail.com>
2025-06-25 00:36:20 -0500

    Version final: Se agrego funcion de editar y eliminar stock con su respectiva logica

--- inventario/src/main/java/com/empresa/inventario/InventarioApplication.java
+++ inventario/src/main/java/com/empresa/inventario/InventarioApplication.java
@@ -8,6 +8,12 @@ public class InventarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
-	}
 
+		try {
+			Thread.sleep(3000);
+			Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
